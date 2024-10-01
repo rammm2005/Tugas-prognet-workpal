be7d6c98b7f3993191d8baa5f11192a24b425f28
IRay3477 <ray.adhya7@gmail.com>
2024-10-01 17:03:14 +0800

    nih baru

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,5 +15,6 @@ class RoleSeeder extends Seeder
     {
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'user']);
+        Role::create(['name' => 'super_admin']);
     }
 }
